dd366e882ff6e02a8eaeff68229508b47ab3f0b8
Jawadsg001 <jawadsegaoui@gmail.com>
2023-09-07 18:02:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*main - entry poit
-*Return: always 0 (success)
-*/
+ *  * main - main block
+ *   * Return: 0
+ *    */
 int main(void)
 {
-	puts("\"Pogramming is like building a multilingual puzzle");
+	printf("\"Pogramming is like building a multilingual puzzle");
 	return (0);
 }
