354f74bd8912e01f069f3328a4abbd977f1b9f8a
Jawadsg001 <jawadsegaoui@gmail.com>
2023-09-11 16:55:29 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,11 @@
+/*
+ *  * File: 4-print_alphabt.c
+ *   * Auth: Brennan D Baraban
+ *    */
+
+#include <stdio.h>
+
+/**
+ *  * main - Prints the alphabet in lowercase, except for q and e.
+ *   *
+ *    * Return: Alwa
