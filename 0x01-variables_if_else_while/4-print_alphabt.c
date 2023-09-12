b2a94411fc660dd762d53f9da24786060eced466
Jawadsg001 <jawadsegaoui@gmail.com>
2023-09-11 17:04:36 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,25 @@
-/*
- *  * File: 4-print_alphabt.c
- *   * Auth: Brennan D Baraban
- *    */
-
 #include <stdio.h>
 
 /**
- *  * main - Prints the alphabet in lowercase, except for q and e.
- *   *
- *    * Return: Alwa
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		if ((c == 'q' || c == 'e') != 1)
+		{
+			putchar(c);
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
+
